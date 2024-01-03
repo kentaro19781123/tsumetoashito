f6d3dc1759a6c575c4ad21e4fef51e1ba166c115
kentaro19781123 <kens19781123@gmail.com>
2024-01-03 15:51:21 +0900

    lint fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
+import { Provider } from "jotai";
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
 import "normalize.css";
 import "./globals.css";
-import { Provider } from "jotai";
-// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
