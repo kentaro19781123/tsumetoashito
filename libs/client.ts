8cad015ba5fbf6bd4da3f084ac1c0fce8f7335a2
kentaro19781123 <kens19781123@gmail.com>
2024-03-15 00:28:32 +0900

    deploy設定

--- libs/client.ts
+++ libs/client.ts
@@ -4,12 +4,12 @@ if (!process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN) {
   throw new Error("NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN is required");
 }
 
-if (!process.env.NEXT_PUBLIC_MICROCMS_GET_API_KEY) {
-  throw new Error("NEXT_PUBLIC_MICROCMS_GET_API_KEY is required");
+if (!process.env.NEXT_PUBLIC_MICROCMS_API_KEY) {
+  throw new Error("NEXT_PUBLIC_MICROCMS_API_KEY is required");
 }
 
 // Initialize Client SDK.
 export const client = createClient({
   serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN,
-  apiKey: process.env.NEXT_PUBLIC_MICROCMS_GET_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY,
 });
