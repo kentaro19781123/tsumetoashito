98b5b302a5ba3c5b8649d4aeb159a9f535adfb39
kentaro19781123 <kens19781123@gmail.com>
2024-08-06 18:27:40 +0900

    build時のwarning解消

--- app/case/preview/page.tsx
+++ app/case/preview/page.tsx
@@ -1,6 +1,11 @@
+import { Suspense } from "react";
 import { CasePreview } from "./preview";
 
 // ページ
 export default async function Page() {
-  return <CasePreview />;
+  return (
+  <Suspense>
+    <CasePreview />
+  </Suspense>
+  )
 }
